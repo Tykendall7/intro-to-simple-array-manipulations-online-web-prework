bb06cc6264a11c0d3f7ded8b1eb0e532d98907e1
Tymeckia Kendall <tykend@gmail.com>
2018-12-01 05:42:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,62 @@
+def using_push 
+     countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+    next_country = "Niger" 
+  
+end
+
+
+def using_unshift
+      neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+    @new_neighborhood = "Brooklyn Heights"
+end
+
+
+def using_pop
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+end
+
+def pop_with_args
+      @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+end
+
+def using_shift
+      neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+end
+
+def shift_args
+  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  
+end
+
+
+def using_concat
+      @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["mario kart", "flatiron school"]
+  
+end
+
+def using_insert
+      @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+    @another_esoteric_language = "Malbolge"
+end
+
+
+def using_uniq
+      captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+  
+end
+
+
+def using_flatten
+       private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+end
+
+def using_delete
+      instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+end
+
+def using_delete_at
+      famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+  
+end
+
